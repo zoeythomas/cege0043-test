2ad9d210e3c8216892591f0597297990b9965097
zoeythomas <ucfscde@ucl.ac.uk>
2019-02-28 12:17:02 -0500

    send information to the serve

--- uploadData.js
+++ uploadData.js
@@ -1,3 +1,8 @@
 function startDataUpload() {
+var name = document.getElementById("name").value;
+var surname = document.getElementById("surname").value;
+var module = document.getElementById("module").value;
+var postString = "name="+name +"&surname="+surname+"&module="+module;
+alert (postString);
 alert ("start data upload");
 }
