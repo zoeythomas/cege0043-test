3ab635b29ab68cf63a640e1f2cb8b85f295315ba
zoeythomas <ucfscde@ucl.ac.uk>
2019-02-28 15:31:45 -0500

    manage checking box etc.

--- uploadData.js
+++ uploadData.js
@@ -9,6 +9,25 @@ var surname = document.getElementById("surname").value;
 var module = document.getElementById("module").value;
 var postString = "name="+name +"&surname="+surname+"&module="+module;
 alert ("start data upload");
+// now get the checkbox values - separate them with a | so that they can be split later on if necessary
+var checkString = "";
+for (var i = 1;i< 5;i++){
+if (document.getElementById("check"+i).checked === true) {
+checkString = checkString +
+document.getElementById("check"+i).value + "||"
+}
+}
+postString = postString + "&modulelist="+checkString;
+// now get the radio button values
+if (document.getElementById("morning").checked) {
+postString=postString+"&lecturetime=morning";
+}
+if (document.getElementById("afternoon").checked) {
+postString=postString+"&lecturetime=afternoon";
+}
+// now get the select box values
+var language = document.getElementById("languageselectbox").value;
+postString = postString + "&language="+language;
 processData(postString);
 }
 
