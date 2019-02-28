a634839c4f39c3db7150f4946af5343c34f9d465
zoeythomas <ucfscde@ucl.ac.uk>
2019-02-28 12:52:02 -0500

    Modify

--- uploadData.js
+++ uploadData.js
@@ -1,9 +1,10 @@
-function startDataUpload() {
 var name = document.getElementById("name").value;
 var surname = document.getElementById("surname").value;
 var module = document.getElementById("module").value;
 var postString = "name="+name +"&surname="+surname+"&module="+module;
 alert (postString);
+
+function startDataUpload() {
 alert ("start data upload");
 processData(postString);
 }
