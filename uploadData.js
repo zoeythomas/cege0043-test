63e6c61b7aad2c457c5e1d17e075f845b19da565
zoeythomas <ucfscde@ucl.ac.uk>
2019-02-28 12:37:52 -0500

    AJAX request

--- uploadData.js
+++ uploadData.js
@@ -6,3 +6,22 @@ var postString = "name="+name +"&surname="+surname+"&module="+module;
 alert (postString);
 alert ("start data upload");
 }
+
+var client; // the global variable that holds the request
+function processData(postString) {
+client = new XMLHttpRequest();
+client.open('POST','http://developer.cege.ucl.ac.uk:30250/reflectData',true);
+client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+client.onreadystatechange = dataUploaded;
+client.send(postString);
+p}
+// create the code to wait for the response from the data server, and process the response once it is received
+
+function dataUploaded() {
+// this function listens out for the server to say that the data is ready - i.e.
+has state 4
+if (client.readyState == 4) {
+// change the DIV to show the response
+document.getElementById("dataUploadResult").innerHTML = client.responseText;
+}
+}
